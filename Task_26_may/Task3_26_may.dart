a6f3db0c3148e0066a4ab43596ced6f07207ed83
Hiren <chauhanhirenec1@gmail.com>
2026-06-08 18:27:27 +0530

    change Task3_26_may

---
+++ Task_26_may/Task3_26_may.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Enter Number : ");
+  var num = int.parse(stdin.readLineSync().toString());
+
+  var reverseNum = "";
+
+  while (num > 0) {
+    reverseNum = "$reverseNum${num % 10}";
+    num ~/= 10;
+  }
+
+  print("Reverse number of given number is ${int.parse(reverseNum)}");
+}
