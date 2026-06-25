8f0e3fa82b0301c6337c9eaae2d20bd5ba6f2f5c
Hiren <chauhanhirenec1@gmail.com>
2026-06-25 18:20:35 +0530

    Session 2 : IDE Configuration & Emulator Setup

---
+++ Session wise Task/Flutter & Dart Environment Setup and Programming/Session 2 - IDE Configuration & Emulator Setup/task_5.dart
@@ -0,0 +1,24 @@
+
+void main() {
+  // 5.
+  // List three differences you noticed between running your Flutter app on the emulator
+  // versus a real Android device. For each difference, explain how it might affect your development or
+  // testing process.
+  // <br><br><em><strong>Hint:</strong> Consider speed, device features, and debugging experience.</em>
+
+
+  // 1. Speed →
+  // Difference : emulator may be slower; real device shows actual performance.
+  // Effect : Testing on a real device may take less time than using an emulator.
+
+  // 2. Hardware/sensors →
+  // Difference : emulator has limited simulation; real device gives real camera/GPS/biometric behavior.
+  // Effect: A emulator has limitation during testing of features like camera, GPS, bluetooth etc.
+  // But, We can test all features in real device.
+
+  // 3. Debugging/testing convenience →
+  // Difference : emulator is easier for quick testing and switching devices; real device is better for realistic final testing.
+  // Effect : We can test our app for different version of android and different sizes on emulator easily.
+  // Real device is better for final validation before considering the app stable.
+
+}
